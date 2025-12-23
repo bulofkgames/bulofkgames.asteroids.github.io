fcc0726b602e115901a8123b2fbfbc4b168a5ba0
bulofkgames <leodiyu@gmail.com>
2025-12-23 12:44:55 -0300

    Update game.js

--- game.js
+++ game.js
@@ -11,8 +11,8 @@
 /* ================= CONSTANTS ================= */
 
 var MAX_SPEED = 300;
-var ROT_SPEED = 180;
-var ACCELERATION = 150;
+var ROT_SPEED = 80;
+var ACCELERATION = 50;
 var FRICTION = 0.98;
 
 /* ================= INPUT ================= */
