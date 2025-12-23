d68ec1cce4f1c203b43936d8f8c568a318525766
bulofkgames <leodiyu@gmail.com>
2025-12-23 12:30:18 -0300

    YT @BULOFK

--- game.js
+++ game.js
@@ -8,69 +8,53 @@
 ========================================
 */
 
+/* ================= CONSTANTS ================= */
+
+var MAX_SPEED = 300;
+var ROT_SPEED = 180;
+var ACCELERATION = 200;
+var FRICTION = 0.98;
+
 /* ================= INPUT ================= */
 
 var KEY_CODES = {
   32: 'space',
   37: 'left',
   38: 'up',
-  39: 'right',
-  40: 'down'
+  39: 'right'
 };
 
-var KEY_STATUS = { keyDown: false };
+var KEY_STATUS = {};
 for (var code in KEY_CODES) {
   KEY_STATUS[KEY_CODES[code]] = false;
 }
 
-$(window)
-  .on('keydown', function (e) {
-    if (KEY_CODES[e.keyCode]) {
-      e.preventDefault();
-      KEY_STATUS.keyDown = true;
-      KEY_STATUS[KEY_CODES[e.keyCode]] = true;
-    }
-  })
-  .on('keyup', function (e) {
-    if (KEY_CODES[e.keyCode]) {
-      e.preventDefault();
-      KEY_STATUS.keyDown = false;
-      KEY_STATUS[KEY_CODES[e.keyCode]] = false;
-    }
-  });
+window.addEventListener('keydown', function (e) {
+  if (KEY_CODES[e.keyCode]) {
+    e.preventDefault();
+    KEY_STATUS[KEY_CODES[e.keyCode]] = true;
+  }
+});
 
-/* ================= MATRIX ================= */
-
-function Matrix() {
-  this.data = [
-    [1, 0, 0],
-    [0, 1, 0]
-  ];
-
-  this.configure = function (rot, scale, tx, ty) {
-    var r = rot * Math.PI / 180;
-    var s = Math.sin(r) * scale;
-    var c = Math.cos(r) * scale;
-    this.data[0][0] = c;
-    this.data[0][1] = -s;
-    this.data[0][2] = tx;
-    this.data[1][0] = s;
-    this.data[1][1] = c;
-    this.data[1][2] = ty;
-  };
-}
+window.addEventListener('keyup', function (e) {
+  if (KEY_CODES[e.keyCode]) {
+    e.preventDefault();
+    KEY_STATUS[KEY_CODES[e.keyCode]] = false;
+  }
+});
 
 /* ================= SPRITE ================= */
 
 function Sprite() {
   this.visible = false;
+
   this.x = 0;
   this.y = 0;
   this.rot = 0;
   this.scale = 1;
 
   this.vel = { x: 0, y: 0, rot: 0 };
-  this.acc = { x: 0, y: 0, rot: 0 };
+  this.acc = { x: 0, y: 0 };
 
   this.init = function (name, points) {
     this.name = name;
@@ -80,39 +64,57 @@ function Sprite() {
   this.run = function (delta) {
     if (!this.visible) return;
     this.move(delta);
+
     this.context.save();
-    this.configureTransform();
+    this.context.translate(this.x, this.y);
+    this.context.rotate(this.rot * Math.PI / 180);
+    this.context.scale(this.scale, this.scale);
     this.draw();
     this.context.restore();
   };
 
   this.move = function (delta) {
+
+    // acceleration
     this.vel.x += this.acc.x * delta;
     this.vel.y += this.acc.y * delta;
+
+    // friction
+    this.vel.x *= FRICTION;
+    this.vel.y *= FRICTION;
+
+    // speed limit
+    var speed = Math.sqrt(this.vel.x * this.vel.x + this.vel.y * this.vel.y);
+    if (speed > MAX_SPEED) {
+      var scale = MAX_SPEED / speed;
+      this.vel.x *= scale;
+      this.vel.y *= scale;
+    }
+
+    // movement
     this.x += this.vel.x * delta;
     this.y += this.vel.y * delta;
-    this.rot = (this.rot + this.vel.rot * delta) % 360;
 
-    /* wrap screen */
+    // rotation
+    this.rot = (this.rot + this.vel.rot * delta + 360) % 360;
+
+    // screen wrap
     if (this.x < 0) this.x += Game.canvasWidth;
     if (this.x > Game.canvasWidth) this.x -= Game.canvasWidth;
     if (this.y < 0) this.y += Game.canvasHeight;
     if (this.y > Game.canvasHeight) this.y -= Game.canvasHeight;
   };
 
-  this.configureTransform = function () {
-    this.context.translate(this.x, this.y);
-    this.context.rotate(this.rot * Math.PI / 180);
-    this.context.scale(this.scale, this.scale);
-  };
-
   this.draw = function () {
     if (!this.points.length) return;
+
     this.context.beginPath();
     this.context.moveTo(this.points[0], this.points[1]);
+
     for (var i = 2; i < this.points.length; i += 2) {
       this.context.lineTo(this.points[i], this.points[i + 1]);
     }
+
     this.context.closePath();
     this.context.stroke();
   };
@@ -169,9 +171,13 @@ window.onload = function () {
   canvas.height = 600;
 
   var ctx = canvas.getContext('2d');
+
+  // IMPORTANT: remove visual artifacts
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
   ctx.strokeStyle = "#ffffff";
-  ctx.fillStyle = "#ffffff";
   ctx.lineWidth = 2;
+  ctx.lineJoin = "round";
+  ctx.lineCap = "round";
 
   Game.canvasWidth = canvas.width;
   Game.canvasHeight = canvas.height;
@@ -182,10 +188,11 @@ window.onload = function () {
   Sprite.prototype.context = ctx;
 
   var ship = new Sprite();
-  ship.init('ship', [-6,6, 0,-12, 6,6]);
+  ship.init('ship', [-6, 6, 0, -12, 6, 6]);
   ship.visible = true;
   ship.x = Game.canvasWidth / 2;
   ship.y = Game.canvasHeight / 2;
+
   Game.sprites.push(ship);
 
   let last = performance.now();
@@ -194,17 +201,21 @@ window.onload = function () {
     var delta = (now - last) / 16.666;
     last = now;
 
-    ctx.clearRect(0, 0, Game.canvasWidth, Game.canvasHeight);
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    if (KEY_STATUS.left) ship.vel.rot = -180 * delta;
-    else if (KEY_STATUS.right) ship.vel.rot = 180 * delta;
+    // ROTATION
+    if (KEY_STATUS.left) ship.vel.rot = -ROT_SPEED;
+    else if (KEY_STATUS.right) ship.vel.rot = ROT_SPEED;
     else ship.vel.rot = 0;
 
+    // ACCELERATION
     if (KEY_STATUS.up) {
-      ship.acc.x = Math.sin(ship.rot * Math.PI / 180) * 40;
-      ship.acc.y = -Math.cos(ship.rot * Math.PI / 180) * 40;
+      ship.acc.x = Math.sin(ship.rot * Math.PI / 180) * ACCELERATION;
+      ship.acc.y = -Math.cos(ship.rot * Math.PI / 180) * ACCELERATION;
     } else {
-      ship.acc.x = ship.acc.y = 0;
+      ship.acc.x = 0;
+      ship.acc.y = 0;
     }
 
     for (var i = 0; i < Game.sprites.length; i++) {
@@ -212,7 +223,7 @@ window.onload = function () {
     }
 
     Text.renderText("ASTEROIDS HTML5", 28, 180, 60);
-    Text.renderText("PRESS ARROWS TO MOVE", 18, 200, 100);
+    Text.renderText("ARROWS TO MOVE", 18, 240, 100);
 
     requestAnimationFrame(loop);
   }
